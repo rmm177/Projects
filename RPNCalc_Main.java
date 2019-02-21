e3e917c5a2b23d02838e58ed32c9192b0c838a1a
rmm177 <46945235+rmm177@users.noreply.github.com>
2019-02-21 17:04:49 -0600

    Create RPNCalc_Main.java

---
+++ RPNCalc_Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class javaCalculator
+{
+	public static void main(String[] args)
+	{
+		int num;
+		int num1;
+		String operation;
+
+		Scanner input = new Scanner(System.in);
+		System.out.printIn("Enter your operation");
+		operation = op.next();
+
+		//System.out.printIn("Enter the second number");
+		//num1 = input.nextInt();
+
+		//Scanner op = new Scanner(System.in);
+		//System.out.printIn("Please enter operation");
+		//operation = op.next();
+
+		if(operation == "+")
+		{
+			System.out.printIn("Your answer is " + (num + num1));
+		}
+		if(operation == "+")
+		{
+			System.out.printIn("Your answer is " + (num + num1));
+		}
+		if(operation == "+")
+		{
+			System.out.printIn("Your answer is " + (num + num1));
+		}
+		if(operation == "+")
+		{
+			System.out.printIn("Your answer is " + (num + num1));
+		}	
+	}
+}
